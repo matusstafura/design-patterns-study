57604ee2bfc89bd71affd4f94d106f5f7b0f6686
Matus Stafura <kobe21@gmail.com>
2025-01-29 13:56:11 +0100

    Update php.php

--- 01-factory-method/php.php
+++ 01-factory-method/php.php
@@ -1,3 +1,58 @@
 <?php
 
-echo "hello";
+interface PaymentGateway
+{
+    public function pay(): void;
+}
+
+class BankTransfer implements PaymentGateway
+{
+    public function pay(): void
+    {
+        echo "Paid by bank transfer";
+    }
+}
+
+class Card implements PaymentGateway
+{
+    public function pay(): void
+    {
+        echo "Paid by card";
+    }
+}
+
+abstract class Checkout
+{
+    abstract protected function createPaymentGateway(): PaymentGateway;
+
+    public function processPayment(): void
+    {
+        $paymentGateway = $this->createPaymentGateway();
+        $paymentGateway->pay();
+    }
+}
+
+class PayByBankTransfer extends Checkout
+{
+    protected function createPaymentGateway(): PaymentGateway
+    {
+        return new BankTransfer();
+    }
+}
+
+class PayByCard extends Checkout
+{
+    protected function createPaymentGateway(): PaymentGateway
+    {
+        return new Card();
+    }
+}
+
+function run(Checkout $checkout): void
+{
+    $checkout->processPayment();
+}
+
+run(new PayByBankTransfer());
+run(new PayByCard());
+
