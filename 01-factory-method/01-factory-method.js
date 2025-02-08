6f44a138fe49c3b7b314c079b4f65ae1ae44fbee
Matus Stafura <kobe21@gmail.com>
2025-02-08 12:58:29 +0100

    01 js

--- 01-factory-method/01-factory-method.js
+++ 01-factory-method/01-factory-method.js
@@ -1,34 +1,64 @@
+// Product Interface
 class Network {
     login() {
-        throw new Error("not implemented")
+        throw new Error("not implemented");
     }
 }
+
+// Concrete Product
 class Bluesky extends Network {
     login() {
-        console.log("logged to bluesky")
+        console.log("logged to bluesky");
     }
 }
 
+// Concrete Product
 class Reddit extends Network {
     login() {
-        console.log("logged to reddit")
+        console.log("logged to reddit");
     }
 }
 
+// Factory Interface (Abstract Creator)
 class SocialMediaFactory {
-    static send(platform) {
-        if (platform == "bs") {
-            return new Bluesky
-        } else if (platform == "rd") {
-            return new Reddit
-        } else {
-            throw new Error()
-        }
+    create() {
+        throw new Error("not implemented");
     }
 }
 
-const platform = SocialMediaFactory.send("bs")
-platform.login()
+// Concrete Factories
+class BlueskyFactory extends SocialMediaFactory {
+    create() {
+        return new Bluesky();
+    }
+}
 
+// Concrete Factories
+class RedditFactory extends SocialMediaFactory {
+    create() {
+        return new Reddit();
+    }
+}
 
+// Client Code Wrapper
+class SocialMediaManager {
+    constructor(factory) {
+        this.factory = factory;
+    }
 
+    login() {
+        const network = this.factory.create(); // Factory Method creates the object
+        network.login(); // Calls login method of the created object
+    }
+}
+
+// Creating concrete factories
+const blueskyFactory = new BlueskyFactory();
+const redditFactory = new RedditFactory();
+
+// Using the factories via the SocialMediaManager (Client Code)
+const blueskyManager = new SocialMediaManager(blueskyFactory);
+const redditManager = new SocialMediaManager(redditFactory);
+
+blueskyManager.login(); // Output: logged to bluesky
+redditManager.login(); // Output: logged to reddit
