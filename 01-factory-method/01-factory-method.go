fb9b1b0a48f007721c878b73a8e85f57f62b8fe6
Matus Stafura <kobe21@gmail.com>
2025-02-10 08:13:30 +0100

    01 go

--- 01-factory-method/01-factory-method.go
+++ 01-factory-method/01-factory-method.go
@@ -2,52 +2,66 @@ package main
 
 import "fmt"
 
+// PaymentGateway is a product interface
 type PaymentGateway interface {
 	pay(n int)
 }
 
+// Stripe is a concrete product
 type Stripe struct {
 	Amount int
 }
 
+func (s Stripe) pay(n int) {
+	fmt.Print("paid by stripe: ", n)
+}
+
+// Paypal is a concrete product
+type Paypal struct {
+	Amount int
+}
+
+func (p Paypal) pay(n int) {
+	fmt.Print("paid by paypal: ", n)
+}
+
+type PaymentFactory interface {
+	createPayment(amount int) PaymentGateway
+}
+
+type StripeFactory struct{}
+
+func (s StripeFactory) createPayment(amount int) PaymentGateway {
+	return newStripe(amount)
+}
+
+type PaypalFactory struct{}
+
+func (p PaypalFactory) createPayment(amount int) PaymentGateway {
+	return newPaypal(amount)
+}
+
+// Factory method
 func newStripe(n int) *Stripe {
 	return &Stripe{
 		Amount: n,
 	}
 }
 
-func (s Stripe) pay(n int) {
-	fmt.Print("paid by stripe", n)
-}
-
-type Paypal struct {
-	Amount int
-}
-
+// Factory method
 func newPaypal(n int) *Paypal {
 	return &Paypal{
 		Amount: n,
 	}
 }
 
-func (p Paypal) pay(n int) {
-	fmt.Print("paid by paypal")
-}
-
-func NewPaymentGateway(gatewayType string, amount int) PaymentGateway {
-	if gatewayType == "stripe" {
-		return newStripe(amount)
-	} else if gatewayType == "paypal" {
-		return newPaypal(amount)
-	}
-	return nil
-}
-
-func notify(p PaymentGateway, n int) {
-	p.pay(n)
+// Client code
+func processPayment(factory PaymentFactory, amount int) {
+	payment := factory.createPayment(amount)
+	payment.pay(amount)
 }
 
 func main() {
-	s := NewPaymentGateway("stripe", 10)
-	notify(s, 10)
+	processPayment(StripeFactory{}, 100) // Outputs: paid by stripe 100
+	processPayment(PaypalFactory{}, 100) // Outputs: paid by paypal 100
 }
